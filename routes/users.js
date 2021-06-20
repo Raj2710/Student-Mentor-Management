c2cd838a9c39abee04b39ae0a0e34d9dcacd54f6
Raj2710 <nagarajansai2727@gmail.com>
2021-06-20 19:55:54 +0530

    Third Commit

--- routes/users.js
+++ routes/users.js
@@ -34,6 +34,32 @@ router.get("/all-mentors",async(req,res)=>{
   }
 })
 
+router.post("/mentor",async(req,res)=>{
+  const client = await MongoClient.connect(dbUrl);
+  try {
+      const db = client.db("StudentMentor");
+      const user = await db.collection("student").findOne({"studentName":req.body.studentName});
+      res.status(200).json(user);
+  } catch (error) {
+      res.json(error);
+  }finally{
+    client.close();
+  }
+})
+
+router.post("/students",async(req,res)=>{
+  const client = await MongoClient.connect(dbUrl);
+  try {
+      const db = client.db("StudentMentor");
+      const user = await db.collection("mentor").findOne({"mentorName":req.body.mentorName});
+      res.status(200).json(user);
+  } catch (error) {
+      res.json(error);
+  }finally{
+    client.close();
+  }
+})
+
 router.post("/add-mentor",async(req,res)=>{
   const client = await MongoClient.connect(dbUrl);
   try {
@@ -72,4 +98,55 @@ router.post("/add-student",async(req,res)=>{
     client.close();
   }
 })
+
+router.post("/assign-students",async(req,res)=>{
+  const client = await MongoClient.connect(dbUrl);
+  try {
+      const db = client.db("StudentMentor");
+      if(req.body.mentorStudents){//updating mentor Name for all students
+        req.body.mentorStudents.map(async(e)=>{
+          const stu = await db.collection("student").updateOne({"studentName":e},{$set:{"studentMentor":req.body.mentorName}});
+        })
+      }
+      if(req.body.mentorName){//updating students name for selected mentor
+        const men = await db.collection("mentor").findOne({"mentorName":req.body.mentorName});
+        console.log(men.mentorStudents);
+        req.body.mentorStudents.map((i)=>{
+          men.mentorStudents.push(i);
+        })
+        console.log(men.mentorStudents);
+        const update = await db.collection("mentor").updateOne({"mentorName":req.body.mentorName},{$set:{"mentorStudents":men.mentorStudents}});
+      }
+      res.status(200).json({
+        message:"Mentor and Students Mapped Successfully!"
+      })
+  } catch (error) {
+      res.status(500).json(error);
+  }finally{
+    client.close();
+  }
+})
+
+
+router.post("/change-mentor",async(req,res)=>{
+  const client = await MongoClient.connect(dbUrl);
+  try {
+      const db = client.db("StudentMentor");
+      const men = await db.collection("mentor").findOne({"mentorName":req.body.oldMentor});
+      men.mentorStudents.splice(men.mentorStudents.indexOf(req.body.studentName),1);
+      const update = await db.collection("mentor").updateOne({"mentorName":req.body.oldMentor},{$set:{"mentorStudents":men.mentorStudents}});
+      const user = await db.collection("student").updateOne({"studentName":req.body.studentName},{$set:{"studentMentor":req.body.mentorName}});
+      const newmen = await db.collection("mentor").findOne({"mentorName":req.body.mentorName});
+      newmen.mentorStudents.push(req.body.studentName);
+      const newupdate = await db.collection("mentor").updateOne({"mentorName":req.body.mentorName},{$set:{"mentorStudents":newmen.mentorStudents}});
+      res.status(200).json({
+        message:"Mentor Changed Successfully!"
+      })
+  } catch (error) {
+      res.status(500).json(error);
+  }finally{
+    client.close();
+  }
+})
+
 module.exports = router;
